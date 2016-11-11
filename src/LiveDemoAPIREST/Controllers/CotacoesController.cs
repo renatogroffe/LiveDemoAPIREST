a4c19db83336a9adf160582831c70e8097f093ba
Renato Groffe <renato.groffe@yahoo.com.br>
2016-11-11 01:00:40 -0200

    Add project files.

---
+++ src/LiveDemoAPIREST/Controllers/CotacoesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using LiveDemoAPIREST.Data;
+
+namespace LiveDemoAPIREST.Controllers
+{
+    [Route("api/[controller]")]
+    public class CotacoesController : Controller
+    {
+        [HttpGet("{id}")]
+        public Cotacao Get(
+                    [FromServices]CotacoesContext context,
+                    string id)
+        {
+            return context.Cotacoes
+                .Where(c => c.Sigla == id)
+                .FirstOrDefault();
+        }
+    }
+}
