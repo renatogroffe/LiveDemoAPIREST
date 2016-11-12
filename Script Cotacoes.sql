11a148ecc5614e17ec811031f88f301243a6d117
Renato Groffe <renato.groffe@yahoo.com.br>
2016-11-12 08:06:07 -0200

    Add files via upload

---
+++ Script Cotacoes.sql
@@ -0,0 +1,45 @@
+USE ExemploCotacoes
+GO
+
+CREATE TABLE dbo.Cotacoes(
+	Sigla char(3) NOT NULL,
+	NomeMoeda varchar(30) NOT NULL,
+	UltimaCotacao datetime NOT NULL,
+	Valor numeric (18,4) NOT NULL,
+	CONSTRAINT PK_Cotacoes PRIMARY KEY (Sigla)
+)
+GO
+
+
+INSERT INTO dbo.Cotacoes
+           (Sigla
+           ,NomeMoeda
+           ,UltimaCotacao
+           ,Valor)
+     VALUES
+           ('USD'
+           ,'Dólar norte-americano'
+           ,'2016-11-10 16:59'
+           ,3.3614)
+
+INSERT INTO dbo.Cotacoes
+           (Sigla
+           ,NomeMoeda
+           ,UltimaCotacao
+           ,Valor)
+     VALUES
+           ('EUR'
+           ,'Euro'
+           ,'2016-11-10 16:59'
+           ,3.7053)
+
+INSERT INTO dbo.Cotacoes
+           (Sigla
+           ,NomeMoeda
+           ,UltimaCotacao
+           ,Valor)
+     VALUES
+           ('LIB'
+           ,'Libra esterlina'
+           ,'2016-11-10 16:59'
+           ,4.266)
